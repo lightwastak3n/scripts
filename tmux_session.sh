7d0dac549a14118bb67adb27d387675a5314a103
Sasa Trivic <sasatrivic92@gmail.com>
2024-05-13 21:33:56 +0200

    Added vim swap file deleter.

--- tmux_session.sh
+++ tmux_session.sh
@@ -5,13 +5,16 @@ cd ~/Documents/Code/
 session="main"
 tmux new-session -d -s $session
 window=0
-tmux rename-window -t $session:$window "nvim-main"
+tmux rename-window -t $session:$window "nvim"
 window=1
 tmux new-window -t $session:$window -n "run"
 window=2
 tmux new-window -t $session:$window -n "git"
+tmux send-keys -t $session:$window "sshgit"
 window=3
-tmux new-window -t $session:$window -n "nvim-test"
+tmux new-window -t $session:$window -n "nvim-2"
 window=4
+tmux new-window -t $session:$window -n "ssh"
+window=5
 tmux new-window -t $session:$window -n "shell"
 tmux attach-session -t $session:0
